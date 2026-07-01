ef2e303d25f6c785ff1afa32305abc48a4451ef3
AK-asta-75 <abishekkannav@gmail.com>
2026-07-01 22:34:04 +0530

    for my 2nd homework

---
+++ Homework2.java
@@ -0,0 +1,11 @@
+class Homework2
+{
+	public static void main (String[]args)
+	{
+		System.out.println("Dhoni finishes off in style! ");
+		System.out.println("One strong filter coffee");
+		System.out.print("Balance: ₹10 ");
+		System.out.println("Mic off, video off, sleeping ON!");
+		System.out.println("Ee sala cup namdhe");
+	}
+}
