61f928a01c8300844c4b0a57a65d55cb7fed61b4
AK-asta-75 <abishekkannav@gmail.com>
2026-07-01 22:07:58 +0530

    for my 1st homework

---
+++ Homework1.java
@@ -0,0 +1,12 @@
+class Homework1
+{
+	public static void main (String[]args)
+	{
+		System.out.println("Welcome to Java Programming!");
+		System.out.println("i love biriyani ");
+		System.out.println("Java Sundaresan");
+		System.out.println("Life is better with music!");
+		System.out.println("Samosa and chai ready!");
+		System.out.println("No talking, only writing!");
+	}
+}
