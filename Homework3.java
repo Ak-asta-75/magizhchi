f508186fb2f310450baced61ad42a98762049198
AK-asta-75 <abishekkannav@gmail.com>
2026-07-02 21:28:42 +0530

    for my 3rd homework

---
+++ Homework3.java
@@ -0,0 +1,14 @@
+class Homework3
+{
+	public static void main (String[]args)
+	{
+		String name = "Abishek";
+		int age = 18;
+		char grade = 'B';
+		boolean passed = true;
+		System.out.println("Name : "+name);
+		System.out.println("Age : "+age);
+		System.out.println("Grade : "+grade);
+		System.out.println("Passed : "+passed);
+	}
+}
