a571ceb50c9e3a5ef291162fe0951afe90ca985a
AK-asta-75 <abishekkannav@gmail.com>
2026-07-02 23:14:34 +0530

    for my 4th hw

---
+++ Homework4.java
@@ -0,0 +1,13 @@
+class Homework4
+{
+	public static void main (String[]args)
+	{
+		int mark1 = 80;
+		int mark2 = 90;
+		int mark3 = 70;
+		int total = mark1 + mark2 + mark3;
+		double average = total/3;
+		System.out.println("Total : "+total);
+		System.out.println("Average : "+average);
+	}
+}
