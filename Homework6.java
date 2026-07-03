657344818507976dccf42b48f997f26e4321932c
AK-asta-75 <abishekkannav@gmail.com>
2026-07-03 21:13:58 +0530

    6th

---
+++ Homework6.java
@@ -0,0 +1,21 @@
+class Homework6
+{
+	public static void main (String [] args)
+	{
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		int a = 75;
+        int b = 564;
+        int sum = a+b;
+        int difference = a-b;
+        int product = a*b;
+        int division = a/b;
+        System.out.println("Numbers: " + a + " and " + b);
+        System.out.println("------------------------");
+        System.out.println("Addition :        " + sum);
+        System.out.println("Subtraction :     " + difference);
+        System.out.println("Multiplication :  " + product);
+        System.out.println("Division :        " + division);
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		
+	}
+}
