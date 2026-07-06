121d27558b12823b47836bd5ec301af83554f0fd
AK-asta-75 <abishekkannav@gmail.com>
2026-07-06 22:30:30 +0530

    9th

---
+++ Homework9.java
@@ -0,0 +1,15 @@
+class Homework9
+{
+    public static void main(String[] args) 
+    {
+        int number = 9;
+        if (number % 2 == 0) 
+        {
+            System.out.println(number + " is an EVEN number.");
+        } 
+        else 
+        {
+            System.out.println(number + " is an ODD number.");
+        }
+    }
+}
