86f9193861eee74c091fa15e75ed9749ada59995
AK-asta-75 <abishekkannav@gmail.com>
2026-07-06 22:43:46 +0530

    10th

---
+++ Homework10.java
@@ -0,0 +1,39 @@
+class Homework10
+{
+    public static void main(String[] args) 
+    {
+        int dayNumber = 6;
+        if (dayNumber == 1) 
+        {
+            System.out.println("Monday");
+        } 
+        else if (dayNumber == 2) 
+        {
+            System.out.println("Tuesday");
+        } 
+        else if (dayNumber == 3) 
+        {
+            System.out.println("Wednesday");
+        } 
+        else if (dayNumber == 4) 
+        {
+            System.out.println("Thursday");
+        } 
+        else if (dayNumber == 5) 
+        {
+            System.out.println("Friday");
+        } 
+        else if (dayNumber == 6) 
+        {
+            System.out.println("Saturday");
+        } 
+        else if (dayNumber == 7) 
+        {
+            System.out.println("Sunday");
+        } 
+        else 
+        {
+            System.out.println("Invalid input! Please enter a number between 1 and 7.");
+        }
+    }
+}
