3461979fe858a9de0d898b795e40db778f8678cd
Ak-asta-75 <abishekkannav@gmail.com>
2026-07-14 10:35:41 +0530

    16th

---
+++ Homework16.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Homework16 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("\n--- ARRAY ANALYSIS RESULTS ---");
+
+        displayArray(array);
+
+        int sum = findSum(array);
+        System.out.println("Sum of array elements: " + sum);
+		
+        int largest = findLargest(array);
+        System.out.println("Largest element: " + largest);
+		
+        int smallest = findSmallest(array);
+        System.out.println("Smallest element: " + smallest);
+
+        scanner.close();
+    }
+    public static void displayArray(int[] arr) {
+        System.out.print("Array elements: [");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+    public static int findSum(int[] arr) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum;
+    }
+    public static int findLargest(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static int findSmallest(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
