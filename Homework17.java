5f6d8596f31c61c64e67363500910d360d17c09f
Ak-asta-75 <abishekkannav@gmail.com>
2026-07-18 00:38:38 +0530

    17th

---
+++ Homework17.java
@@ -0,0 +1,21 @@
+class Homework17{
+	public static void main(String[]args){
+		System.out.println("int to double");
+		int a = 25;
+		double b = a;
+		
+		System.out.println(b);
+		
+		// character to ascii
+		System.out.println("char to ASCII");
+		char c = 'B';
+		int d = c;
+		System.out.println(d);
+		
+		//explicit casting
+		System.out.println("explicit casting");
+		double e = 45.89;
+		int f = (int) e;
+		System.out.println(f);
+	}
+}
