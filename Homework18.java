a619fe06d50c58fb34d1682706809cfec5278efb
Ak-asta-75 <abishekkannav@gmail.com>
2026-07-21 09:48:17 +0530

    18th

---
+++ Homework18.java
@@ -0,0 +1,15 @@
+class Homework18
+{
+	public static void main(String[]args)
+	{
+		int a = 75;
+		Homework18 obj = new Homework18();
+		obj.passValue(a);
+		System.out.println(a);
+	}
+	public void passValue(int b)
+	{
+		b = 564;
+		System.out.println(b);
+	}
+}
